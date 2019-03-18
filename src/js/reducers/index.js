39cb79bf9b6977a70ddbdd3a8b5cac78c58b729a
prateek-patel <patel.prateek26@gmail.com>
2019-03-18 08:50:25 +0530

    created redux store

---
+++ src/js/reducers/index.js
@@ -0,0 +1,22 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
+const initialState = {
+    articles: []
+};
+
+function rootReducer (state = initialState, action) {
+    if(action.type === ADD_ARTICLE) {
+        return Object.assign({}, state, {articles: state.articles.concat(action.payload)
+        });
+    }
+    return state;
+}
+
+export default rootReducer;
+
+/**
+ * There are two key points for avoiding mutations in Redux:
+ *
+ * Using concat(), slice(), and …spread for arrays
+ * Using Object.assign() and …spread for objects
+ */
